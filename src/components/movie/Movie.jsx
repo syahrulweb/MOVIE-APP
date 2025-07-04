2558ac1ea49392d5439827c1e3799501fb656764
syahrulweb <sahrulloji05@gmail.com>
2025-07-04 07:43:16 +0700

    Finadjjdjd

--- src/components/movie/Movie.jsx
+++ src/components/movie/Movie.jsx
@@ -1,16 +1,30 @@
 import StyledMovie from "./Movie.styled";
-import { Link } from "react-router-dom"; 
+import { Link } from "react-router-dom";
 
 function Movie(props) {
   const { movie } = props;
-  const tmdbImage = `http://image.tmdb.org/t/p/w300/${movie.poster_path}`;
-  const year = movie.year || movie.release_date;
+
+  // Pastikan Anda menggunakan properti yang benar dari OMDB API
+  // OMDB memberikan URL poster lengkap di properti 'Poster'
+  // Jika movie.Poster adalah "N/A" atau kosong, gunakan gambar placeholder
+  const imageUrl = movie.Poster && movie.Poster !== "N/A"
+                   ? movie.Poster
+                   : "https://via.placeholder.com/300x450?text=No+Poster"; // Ganti dengan URL placeholder Anda jika ada
+
+  // OMDB biasanya memiliki properti 'Title' dan 'Year'
+  const title = movie.Title;
+  const year = movie.Year;
+
+  // OMDB menggunakan 'imdbID' untuk detail film
+  const id = movie.imdbID;
 
   return (
     <StyledMovie>
-      <img src={movie.poster || tmdbImage} alt={movie.title} />
-      <Link to={`/movie/${movie.id}`}> 
-        <h3>{movie.title}</h3>
+      {/* Menggunakan imageUrl yang sudah ditangani kasus 'N/A' */}
+      <img src={imageUrl} alt={title} />
+      {/* Menggunakan Link ke detail berdasarkan imdbID */}
+      <Link to={`/movie/${id}`}>
+        <h3>{title}</h3>
       </Link>
       <p>{year}</p>
     </StyledMovie>
