1ab9a694858201829010432aeafd7ce87bb30f95
syahrulweb <sahrulloji05@gmail.com>
2025-07-04 07:47:45 +0700

    Finadjjdsaw21dddd

--- src/components/movie/Movie.jsx
+++ src/components/movie/Movie.jsx
@@ -1,16 +1,29 @@
 import StyledMovie from "./Movie.styled";
-import { Link } from "react-router-dom"; 
+import { Link } from "react-router-dom";
 
 function Movie(props) {
-  const { movie } = props;
-  const tmdbImage = `http://image.tmdb.org/t/p/w300/${movie.poster_path}`;
-  const year = movie.year || movie.release_date;
+  const { movie } = props; // Objek 'movie' ini diharapkan dari TMDB API
+
+  // Base URL TMDB untuk gambar poster
+  const TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w300";
+
+  // Menentukan URL gambar yang akan ditampilkan:
+  // 1. Jika movie.poster_path ada dan valid dari TMDB, gunakan itu.
+  // 2. Jika tidak, gunakan URL placeholder.
+  const imageUrlToDisplay = movie.poster_path
+                            ? `${TMDB_IMAGE_BASE_URL}${movie.poster_path}`
+                            : "https://via.placeholder.com/300x450?text=No+Poster"; // Gunakan URL placeholder yang lengkap dan valid
+
+  // Properti film dari TMDB API:
+  const title = movie.title;           // TMDB menggunakan 'title' (huruf kecil)
+  const year = movie.release_date ? movie.release_date.substring(0, 4) : 'N/A'; // TMDB menggunakan 'release_date'
+  const id = movie.id;                 // TMDB menggunakan 'id' untuk ID unik
 
   return (
     <StyledMovie>
-      <img src={movie.poster || tmdbImage} alt={movie.title} />
-      <Link to={`/movie/${movie.id}`}> 
-        <h3>{movie.title}</h3>
+      <img src={imageUrlToDisplay} alt={title} />
+      <Link to={`/movie/${id}`}>
+        <h3>{title}</h3>
       </Link>
       <p>{year}</p>
     </StyledMovie>
